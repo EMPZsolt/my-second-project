d076fadd514ef0bc8097ef44f14720789e9c07ea
Zsolt Gulyas <zsolt.gulyas86@gmail.com>
2023-06-06 13:01:58 +0000

    Add constants for the DOM elements and array of choices

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+/**
+ * Declare constants for the DOM elements
+ * and possible choices
+ */
+
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = documnet.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors"];
+
